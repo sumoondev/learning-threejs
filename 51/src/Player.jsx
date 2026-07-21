b62d8e760543400bd34c9abf051e7e3c537ad597
sumoondev <sumoon.dev@gmail.com>
2026-07-21 23:30:59 +0545

    feat: create bound and player with controls

---
+++ 51/src/Player.jsx
@@ -0,0 +1,96 @@
+import { useRapier, RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+import { useKeyboardControls } from '@react-three/drei'
+import { useEffect, useRef } from 'react'
+
+export default function Player()
+{
+    const body = useRef()
+    const [ subscribeKeys, getKeys ] = useKeyboardControls()
+    const { rapier, world } = useRapier()
+    const rapierWorld = world.raw()
+
+    const jump = () =>
+    {
+        const origin = body.current.translation()
+        origin.y -= 0.31
+        const direction = { x: 0, y: - 1, z: 0 }
+        const ray = new rapier.Ray(origin, direction)
+        const hit = rapierWorld.castRay(ray, 10, true)
+
+        if(hit.toi < 0.15)
+        {
+            body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+        }
+
+    }
+
+    useEffect(() =>
+    {
+        const unsubscribeJump = subscribeKeys(
+            (state) => state.jump,
+            (value) =>
+            {
+                if(value)
+                {
+                    jump()
+                }
+            }
+        )
+
+        return () =>
+        {
+            unsubscribeJump()
+        }
+    }, [])
+
+    useFrame((state, delta) =>
+    {
+        const { forward, backward, leftward, rightward } = getKeys()
+
+        const impulse = { x: 0, y: 0, z: 0 }
+        const torque = { x: 0, y: 0, z: 0 }
+
+        const impulseStrength = 0.3 * delta
+        const torqueStrength = 0.1 * delta
+
+        if(forward)
+        {
+            impulse.z -= impulseStrength
+            torque.x -= torqueStrength
+        }
+        if(rightward)
+        {
+            impulse.x += impulseStrength
+            torque.z -= torqueStrength
+        }
+        if(backward)
+        {
+            impulse.z += impulseStrength
+            torque.x += torqueStrength
+        }
+        if(leftward)
+        {
+            impulse.x -= impulseStrength
+            torque.z += torqueStrength
+        }
+
+        body.current.applyImpulse(impulse)
+        body.current.applyTorqueImpulse(torque)
+    })
+
+    return <RigidBody 
+            ref={ body } 
+            colliders="ball" 
+            restitution={ 0.2 } 
+            friction={ 1 } 
+            linearDamping={ 0.5 }
+            angularDamping={ 0.5 }
+            position={ [ 0, 1, 0 ] } 
+        >
+        <mesh castShadow >
+            <icosahedronGeometry args={ [ 0.3, 1 ] } />
+            <meshStandardMaterial flatShading color="mediumpurple" />
+        </mesh>
+    </RigidBody>
+}
